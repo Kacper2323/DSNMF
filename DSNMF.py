1e7fbc3322737842a5024848a6a1e45f5aac8a3c
Kacper <kacper.biernat123@gmail.com>
2023-09-10 21:52:09 +0200

    translation PL to ENG

--- DSNMF.py
+++ DSNMF.py
@@ -28,13 +28,22 @@ class DSNMF(torch.nn.Module):
   def loss(self, y, y_pred):
     return ((y - y_pred)**2).sum()
 
+  def getsum(self):
+    #get final approximation
+    wynik = self.Zparams[0]
+    for i in self.Zparams[1:]:
+      wynik = torch.matmul(wynik, i)
+    return wynik.detach().numpy()
+
   def pretrain(self, layers, epochs):
     #method for pretraining layers, H[n] = Z[n+1] * H[n+1]
     #layers - number of layers in decomposition
     #epochs - vector; number of epochs for each layer
     M = torch.tensor(self.dane)
     x = 0
+
     for j in range(layers):
+
       print("LAYER {}".format(j))
       Z, H = init_weights(3, 3) #size of matrices Z=mxm, H=mxn
       pretrain_params = [torch.tensor(Z, requires_grad=True), torch.tensor(H, requires_grad=True)]
@@ -49,17 +58,20 @@ class DSNMF(torch.nn.Module):
         with torch.no_grad():
           pretrain_params[-1] = relu(pretrain_params[-1]) #force non-negativity on H matrix
         print("Pretrain epoch {}. Residual [{:.6f}]".format(i, l), end="\r")
+
       x+=1
       print("\n")
       self.Zparams.append(pretrain_params[0].detach().clone())
       self.Hparams.append(pretrain_params[1].detach().clone())
       M = pretrain_params[1].clone()
 
-  def uczenie(self, epochs):
+  def train(self, epochs):
     #final, all layers learning
+
     print("LEARNING")
     y = torch.tensor(self.dane)
     optimizer = torch.optim.Adam(self.Zparams, lr=1e-2, betas=(0.97, 0.999), eps=1e-16)
+
     self.Zparams.append(self.Hparams[-1])
     for i in self.Zparams:
       i.requires_grad_()
@@ -73,11 +85,5 @@ class DSNMF(torch.nn.Module):
       with torch.no_grad():
         self.Zparams[-1] = relu(self.Zparams[-1])
       print("Pretrain epoch {}. Residual [{:.6f}]".format(i, l), end="\r")
-    print('\n')
 
-  def getsum(self):
-    #get final approximation
-    wynik = self.Zparams[0]
-    for i in self.Zparams[1:]:
-      wynik = torch.matmul(wynik, i)
-    return wynik.detach().numpy()
+    print('\n')
